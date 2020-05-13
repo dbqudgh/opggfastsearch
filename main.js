f2af9ddb4f7096b1317007549c909b2a3fcf9f17
dbqudgh <60841247+dbqudgh@users.noreply.github.com>
2020-05-13 15:58:45 +0900

    코드 수정

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ ROX Peanut 님이 방에 참가했습니다.
 ROX Kuro 님이 방에 참가했습니다.
 ROX PraY 님이 방에 참가했습니다.
 ROX GorillA 님이 방에 참가했습니다.
+
+
+페이커 김치도둑님이 로비에 참가하셨습니다.
+ehcl2048님이 로비에 참가하셨습니다.
+혼모노9999님이 로비에 참가하셨습니다.
+Ryanblak님이 로비에 참가하셨습니다.
+니아비뭐하시노님이 로비에 참가하셨습니다.
+
 */
 const input = document.querySelector('input');
 const baseUrl = "https://www.op.gg/multi/query="
@@ -14,12 +22,12 @@ input.addEventListener('change',(e)=>{
     let result = '';
 
     const text = input.value
-    const list = text.split('님이 방에 참가했습니다.')
-    console.log(list.length)
-    for(let i = 0; i < list.length; i++){
-        result = result+list[i]+",";
+    const list = text.split('님이 로비에 참가하셨습니다.')
+
+    for(let i = 0; i < list.length-1; i++){
+        result += list[i]+","
     }
+    console.log(result)
 
-
-    // window.open(baseUrl+result);
+    window.open(baseUrl+result);
 })
